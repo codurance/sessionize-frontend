5b306f68017cd5d781c818adf8e3c531336b9321
sb48 <sophiebiber@icloud.com>
2021-05-17 15:36:17 +0100

    Add nock test for authentication fetch call

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import GoogleLogin, {GoogleLoginResponse, GoogleLoginResponseOffline} from "react-google-login";
 const base_url = "http://localhost:8080/";
 const auth_url = "auth"
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const responseGoogle = (response : GoogleLoginResponse | GoogleLoginResponseOffline) => {
     console.log(response);
-    fetch(base_url + auth_url, {
-      method: 'POST',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
+    if ("profileObj" in response) {
+      fetch(base_url + auth_url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
           'Content-Type': 'application/json'
-      },
-      referrerPolicy: 'no-referrer',
-      body: JSON.stringify((response as GoogleLoginResponse).profileObj.email)
-    });
+        },
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify({email : response.profileObj.email})
+      }).then(r => r.status === 200
+          ? setIsAuthenticated(true)
+          : setIsAuthenticated(false))
+          .catch(error => console.log(error));
+    }
+
   }
-
-
 
   return (
     <div className="App">
+      {
+        isAuthenticated ? "signedIn"
+            :
         <GoogleLogin
           clientId="614607322794-3d224njddvnkmimjf8hpudt3e1eou3tg.apps.googleusercontent.com"
           buttonText="Login with Google"
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
           cookiePolicy={'single_host_origin'}
-        />
+      />}
     </div>
   );
 }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,22 @@
-import {render} from '@testing-library/react'
+import {getByText, render} from '@testing-library/react'
 import App from "../App";
 import React from "react";
+import userEvent from "@testing-library/user-event";
+
+const nock = require('nock');
 
 describe('App', () => {
-  it('should render the button', () => {
-    const {getByRole} = render(<App />)
-    expect(getByRole('button',  {name: 'Login with Google'})).toBeInTheDocument()
-  })
+    it('should render the button', () => {
+        const {getByRole} = render(<App/>)
+        expect(getByRole('button', {name: 'Login with Google'})).toBeInTheDocument()
+    })
+    it('should successfully authenticate', () => {
+        nock('http://localhost:8080/')
+            .post('auth', {email: "testEmail"})
+            .reply(200)
+        const {getByText, getByRole} = render(<App/>)
+        userEvent.click(getByRole('button', {name: 'Login with Google'}))
+        expect(getByText("signedIn")).toBeInTheDocument()
+    })
 })
+
