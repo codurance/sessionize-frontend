73e07f06faf7868e051c31e607386d6b20034b98
anddak <andras.dako@gmail.com>
2021-05-19 15:37:44 +0100

    updated get payload

--- src/api/get.ts
+++ src/api/get.ts
@@ -1,12 +1,11 @@
 const get = async (url: string, options : any) => {
   return await fetch(url, {
     method: 'GET',
-    mode: 'cors',
     cache: 'no-cache',
     credentials: 'same-origin',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${options.tokenId}`
+      'Authorization': `${options.tokenId}`
     },
     referrerPolicy: 'no-referrer',
   })
