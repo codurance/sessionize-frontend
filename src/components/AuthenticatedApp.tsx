9f2893bbaa8de38056005621bd314a26d777f36a
anddak <andras.dako@gmail.com>
2021-05-20 16:52:07 +0100

    add gooogle logout

--- src/components/AuthenticatedApp.tsx
+++ src/components/AuthenticatedApp.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { GoogleLogout } from 'react-google-login';
 
 export const AuthenticatedApp = ({user, logout} : any) => {
   return (
     <>
       <h1>
-        {`Hello, ${user.name}!`}
+        {`Hello, ${user.email}!`}
       </h1>
-      <button onClick={logout}>
-        Logout
-      </button>
+      <GoogleLogout
+        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        buttonText="Logout"
+        onLogoutSuccess={logout}
+      >
+      </GoogleLogout>
     </>
   )
 }
